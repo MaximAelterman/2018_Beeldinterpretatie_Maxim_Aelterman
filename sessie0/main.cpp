6f5737012df3b6b90c7b1ff74849f91f0618c260
MaximAelterman <maximaelterman@gmail.com>
2018-10-24 21:01:56 +0200

    Aanpassen van readme + laatste aanpassingen code

--- sessie0/main.cpp
+++ sessie0/main.cpp
@@ -5,13 +5,8 @@ using namespace std;
 using namespace cv;
 
 void readImage(string image){
-//Grayscale
-    Mat img_gray = imread(image, IMREAD_GRAYSCALE);
-    namedWindow("Grayscale", WINDOW_AUTOSIZE);
-    imshow("Grayscale", img_gray);
-
 //Colored
-    Mat img_color = imread(image, 1);
+    Mat img_color = imread(image);
     namedWindow("Colored", WINDOW_AUTOSIZE);
     imshow("Colored", img_color);
 
@@ -28,8 +23,24 @@ void readImage(string image){
         //Red
     namedWindow("Red channel", WINDOW_AUTOSIZE);
     imshow("Red channel", channels[2]);
+
+//Grayscale
+    Mat img_gray;
+    cvtColor(img_color, img_gray, COLOR_BGR2GRAY);      //convert to image to grayscale
+    namedWindow("Grayscale", WINDOW_AUTOSIZE);
+    imshow("Grayscale", img_gray);
+
+    int rows = img_gray.rows;
+    int cols = img_gray.cols;
+
+    for(int row = 0; row < rows; row++){
+        for(int col = 0; col < cols; col++){
+            cout << img_gray.at<u_int>(row, col) << " ";
+        }
+        cout << endl;
+    }
+
     cout << "image read" << endl;
-    waitKey(0);
     return;
 }
 
@@ -45,34 +56,39 @@ int main(int argc, const char**argv)
         return 0;
     }
 
-//argumenten inlezen
+//capture arguments
     string image_location(parser.get<string>("@image"));
 
     cout << "Image location: " << image_location << endl;
 
     Mat img = imread(image_location, IMREAD_COLOR);
 
-    Mat drawing = Mat::zeros(400, 400, CV_8UC3);
-    line(drawing, Point(15, 20), Point(70, 50), Scalar(0, 0, 255), 2, 8);
+//self-made drawing with opencv draw functions
+    Mat drawing = Mat(177, 285, CV_8UC3, Scalar(255, 255, 255));        //White surface the size of the images
+    rectangle(drawing, Point(40, 60), Point(250, 160), Scalar(255, 0, 0), 2, 8);
+    line(drawing, Point(40, 60), Point(145, 10), Scalar(255, 0, 0), 2, 8);
+    line(drawing, Point(145, 10), Point(250, 60), Scalar(255, 0, 0), 2, 8);
+    circle(drawing, Point(20, 20), 30, Scalar(0, 255, 255), -1, 8);     //negative thickness: filled circle
     namedWindow("Drawing", WINDOW_AUTOSIZE);
     imshow("Drawing", drawing);
 
     if(img.empty()){
         cerr << "Image not found." << endl;
         parser.printMessage();
-        return -1;
+        //return -1;
     }
 
     if(image_location.empty()){
         cerr << "Something went wrong with your arguments" << endl;
         parser.printMessage();
-        return -1;
+        //return -1;
     }
 
     if(parser.has("@image")){
         readImage(image_location);
-        return 0;
+        //return 0;
     }
 
+    waitKey(0);         //wait after everything is drawn so we can see the results
     return 0;
 }
