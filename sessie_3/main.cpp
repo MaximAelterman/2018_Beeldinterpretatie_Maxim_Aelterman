bb203e6e4b85f81483dd4c4c3d4b8f37b2e52d3a
MaximAelterman <maximaelterman@gmail.com>
2018-11-18 11:18:46 +0100

    multidetectie toegevoegd

--- sessie_3/main.cpp
+++ sessie_3/main.cpp
@@ -9,17 +9,24 @@ int main(int argc, const char**argv)
 {
     CommandLineParser parser(argc, argv,
     "{ help h || show this message }"
-    "{ @image i || path to color image }"
-    "{ @template t || path to template }"
+    "{ @image || path to color image }"
+    "{ @template || path to template }"
     );
+
+    if(parser.has("help")){
+        parser.printMessage();
+        return 0;
+    }
 
     string imgPath = parser.get<string>("@image");
     string templatePath = parser.get<string>("@template");
 
         if(imgPath.empty() || templatePath.empty()){
-        cerr << "Something went wrong with your arguments" << endl;
+        cerr << "Something went wrong with your arguments: --h for help" << endl;
         return -1;
     }
+
+    cout << "opening " << imgPath << " as image and " << templatePath << " as template..." << endl;
 
     Mat img = imread(imgPath);
     Mat templ = imread(templatePath);
@@ -31,30 +38,36 @@ int main(int argc, const char**argv)
     namedWindow("Template", WINDOW_AUTOSIZE);
     imshow("Template", templ);
 
-    Mat img_display;
-    img.copyTo( img_display );
+        //convert to grayscale
+    cvtColor(img, img, COLOR_BGR2GRAY);
+    cvtColor(templ, templ, COLOR_BGR2GRAY);
 
-    //int match_method  = CV_TM_SQDIFF;
-    int match_method  = TM_SQDIFF;
+    Mat img_display;
+    img.copyTo(img_display);
+
+    int match_method  = TM_CCOEFF_NORMED;    //gives the best results
 
         // Create the result matrix
     int result_cols =  img.cols - templ.cols + 1;
     int result_rows = img.rows - templ.rows + 1;
 
-    result.create( result_rows, result_cols, CV_32FC1 );
+    result.create(result_rows, result_cols, CV_32FC1);
 
         // Do the Matching and Normalize
-    matchTemplate( img, templ, result, match_method );
-    normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+    matchTemplate(img, templ, result, match_method);
+    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
         // Localizing the best match with minMaxLoc
-    double minVal; double maxVal; Point minLoc; Point maxLoc;
+    double minVal;
+    double maxVal;
+    Point minLoc;
+    Point maxLoc;
     Point matchLoc;
 
     minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
 
-        // For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-    if(match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED){
+        // CV_TM_SQDIFF and TM_CCOEFF have their best matches in the lowest values the others in the highest values
+    if(match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED || match_method == TM_CCOEFF_NORMED){
         matchLoc = minLoc;
     }
     else{
@@ -62,21 +75,19 @@ int main(int argc, const char**argv)
     }
 
         //rectangle around the found object
-    rectangle( img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
-    rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
-
-
+    rectangle(img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0);
+    rectangle(result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0);
 
         //multiple object matching
-    Mat mask = Mat::zeros(img.rows, img.cols, CV_8UC1);
-    //int tempWidth = templ.cols;
-    //int tempHeight = templ.rows;
+    Mat mask;
 
     threshold(result, mask, 0.9, 1, THRESH_BINARY);
+    mask.convertTo(mask, CV_8UC1);
+    mask *= 255;                    //the mask has values between 0 and 1 which wont be visible for us
 
 
     vector<vector<Point> > contours;
-    findContours(result, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
 
     if(contours.size() > 0){
         for(int i = 0; i < contours.size(); i++){
@@ -84,20 +95,12 @@ int main(int argc, const char**argv)
             Mat temp = result(region);
             Point maxLoc1;
             minMaxLoc(temp, NULL, NULL, &maxLoc1);
-            rectangle(img_display, Point(region.x + maxLoc1.x, region.y + maxLoc1.y), Point(maxLoc1.x + region.x + templ.cols, region.y + maxLoc.y), Scalar::all(0), 2, 8, 0);
+            rectangle(img_display, Point(region.x + maxLoc1.x, region.y + maxLoc1.y), Point(maxLoc1.x + region.x + templ.cols, region.y + maxLoc1.y + templ.rows), Scalar::all(0), 2, 8, 0);
         }
     }
-    //rectangle(mask, Point(0-(tempWidth/2), 0-(tempHeight/2)), Point(0+(tempWidth/2), 0+(tempHeight/2)), Scalar::all(255), CV_FILLED, 8, 0);
-    //minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, mask);
 
-    /*
-        findcontours van de template matches om hier een bounding box rond te zetten
-            OF
-        sliding window maar moeilijker
-    */
-
-    //namedWindow("Mask", WINDOW_AUTOSIZE);
-    //imshow("Mask", mask);
+    namedWindow("Mask", WINDOW_AUTOSIZE);
+    imshow("Mask", mask);
 
     namedWindow("Result", WINDOW_AUTOSIZE);
     imshow("Result", img_display);
