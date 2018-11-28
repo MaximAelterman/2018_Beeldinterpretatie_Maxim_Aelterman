2531c8c0c8531cbdc9df7b3b6ceaaa6dafbf0f0a
MaximAelterman <maximaelterman@gmail.com>
2018-11-28 15:38:20 +0100

    opdracht afgewerkt + readme

---
+++ sessie_4/README.md
@@ -0,0 +1,10 @@
+# Sessie 4
+
+Het programma leest een afbeelding in en zoekt matches tussen de afbeelding en het object door middel van ORB, BRISK en AKAZE.
+Indien er een object gevonden is wordt deze aangeduid met een groene kader (enkel bij ORB).
+
+
+Gebruik:
+```
+./sessie_4 --i <path_to_image> --o <path_to_object>
+```
--- sessie_4/main.cpp
+++ sessie_4/main.cpp
@@ -49,18 +49,20 @@ void orb_keypoints(Mat img, Mat obj)
     Mat descriptors_img, descriptors_obj;
 
         //detect the keypoints
-    detector->detectAndCompute(img, Mat(), keypoints_img, descriptors_img);
     detector->detectAndCompute(obj, Mat(), keypoints_obj, descriptors_obj);
+    detector->detectAndCompute(img, Mat(), keypoints_img, descriptors_img);
 
         //bruteforce matching
     BFMatcher matcher(NORM_L2);
     std::vector<DMatch> matches;
-    matcher.match(descriptors_img, descriptors_obj, matches);
 
+    matcher.match(descriptors_obj, descriptors_img, matches);
+
+        //draw all matches
     Mat img_matches;
-    drawMatches(img, keypoints_img, obj, keypoints_obj, matches, img_matches);
+    drawMatches(obj, keypoints_obj, img, keypoints_img, matches, img_matches);
 
-            //mark keypoints on images
+        //mark keypoints on images
     drawKeypoints(img, keypoints_img, orb_img, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
     drawKeypoints(obj, keypoints_obj, orb_obj, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
 
@@ -76,7 +78,7 @@ void orb_keypoints(Mat img, Mat obj)
         //homography through RANSAC
     Mat result;
     double max_dist = 0; double min_dist = 100;
-    for(int i = 0; i < descriptors_img.rows; i++){
+    for(int i = 0; i < descriptors_obj.rows; i++){
         double dist = matches[i].distance;
         if(dist < min_dist) min_dist = dist;
         if(dist > max_dist) max_dist = dist;
@@ -84,19 +86,19 @@ void orb_keypoints(Mat img, Mat obj)
 
         //find the good matches
     vector<DMatch> good_matches;
-    for(int i = 0; i < descriptors_img.rows; i++){
+    for(int i = 0; i < descriptors_obj.rows; i++){
         if(matches[i].distance <= 3*min_dist){
             good_matches.push_back(matches[i]);
         }
     }
-
-        drawMatches(img, keypoints_img, obj, keypoints_obj, good_matches, result);
+        //only draw the good matches
+    drawMatches(obj, keypoints_obj, img, keypoints_img, good_matches, result, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
         //find object in scene
     vector<Point2f> objct, scene;
     for(size_t i = 0; i < good_matches.size(); i++){
         objct.push_back(keypoints_obj[good_matches[i].queryIdx].pt);
-        scene.push_back(keypoints_img[good_matches[i].queryIdx].pt);
+        scene.push_back(keypoints_img[good_matches[i].trainIdx].pt);
     }
 
     Mat H = findHomography(objct, scene, RANSAC);
@@ -104,22 +106,15 @@ void orb_keypoints(Mat img, Mat obj)
         //Draw rectangle around object
     vector<Point2f> obj_corners(4);
     obj_corners[0] = cvPoint(0, 0); obj_corners[1] = cvPoint(obj.cols, 0);
-    obj_corners[2] = cvPoint(0, obj.rows); obj_corners[3] = cvPoint(obj.cols, obj.rows);
+    obj_corners[2] = cvPoint(obj.cols, obj.rows); obj_corners[3] = cvPoint(0, obj.rows);
 
     vector<Point2f> scene_corners(4);
     perspectiveTransform(obj_corners, scene_corners, H);
 
-/*
-    line(result, obj_corners[0], obj_corners[1], Scalar(0, 255, 0), 5, LINE_8, 0);
-    line(result, obj_corners[0], obj_corners[2], Scalar(0, 255, 0), 5, LINE_8, 0);
-    line(result, obj_corners[2], obj_corners[3], Scalar(0, 255, 0), 5, LINE_8, 0);
-    line(result, obj_corners[1], obj_corners[3], Scalar(0, 255, 0), 5, LINE_8, 0);
-*/
-
-    line(result, scene_corners[0] + Point2f((float)obj.cols, 0), scene_corners[1] + Point2f((float)obj.cols, 0), Scalar(0, 255, 0), 2);
-    line(result, scene_corners[1] + Point2f((float)obj.cols, 0), scene_corners[2] + Point2f((float)obj.cols, 0), Scalar(0, 255, 0), 2);
-    line(result, scene_corners[2] + Point2f((float)obj.cols, 0), scene_corners[3] + Point2f((float)obj.cols, 0), Scalar(0, 255, 0), 2);
-    line(result, scene_corners[3] + Point2f((float)obj.cols, 0), scene_corners[0] + Point2f((float)obj.cols, 0), Scalar(0, 255, 0), 2);
+    line(result, scene_corners[0] + Point2f(obj.cols, 0), scene_corners[1] + Point2f(obj.cols, 0), Scalar(0, 255, 0), 4);
+    line(result, scene_corners[1] + Point2f(obj.cols, 0), scene_corners[2] + Point2f(obj.cols, 0), Scalar(0, 255, 0), 4);
+    line(result, scene_corners[2] + Point2f(obj.cols, 0), scene_corners[3] + Point2f(obj.cols, 0), Scalar(0, 255, 0), 4);
+    line(result, scene_corners[3] + Point2f(obj.cols, 0), scene_corners[0] + Point2f(obj.cols, 0), Scalar(0, 255, 0), 4);
 
     namedWindow("ORB matches (RANSAC)", WINDOW_AUTOSIZE);
     imshow("ORB matches (RANSAC)", result);
@@ -137,20 +132,20 @@ void brisk_keypoints(Mat img, Mat obj)
     Mat descriptors_img, descriptors_obj;
 
         //detect the keypoints
-    detector->detectAndCompute(img, Mat(), keypoints_img, descriptors_img);
     detector->detectAndCompute(obj, Mat(), keypoints_obj, descriptors_obj);
+    detector->detectAndCompute(img, Mat(), keypoints_img, descriptors_img);
 
         //bruteforce matching
     BFMatcher matcher(NORM_L2);
     std::vector<DMatch> matches;
-    matcher.match(descriptors_img, descriptors_obj, matches);
+    matcher.match(descriptors_obj, descriptors_img, matches);
 
     Mat img_matches;
-    drawMatches(img, keypoints_img, obj, keypoints_obj, matches, img_matches);
+    drawMatches(obj, keypoints_obj, img, keypoints_img, matches, img_matches);
 
         //mark keypoints on images
-    drawKeypoints(img, keypoints_img, brisk_img, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
     drawKeypoints(obj, keypoints_obj, brisk_obj, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
+    drawKeypoints(img, keypoints_img, brisk_img, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
 
     namedWindow("BRISK image", WINDOW_AUTOSIZE);
     imshow("BRISK image", brisk_img);
@@ -174,20 +169,20 @@ void akaze_keypoints(Mat img, Mat obj)
     Mat descriptors_img, descriptors_obj;
 
         //detect the keypoints
-    detector->detectAndCompute(img, Mat(), keypoints_img, descriptors_img);
     detector->detectAndCompute(obj, Mat(), keypoints_obj, descriptors_obj);
+    detector->detectAndCompute(img, Mat(), keypoints_img, descriptors_img);
 
         //bruteforce matching
     BFMatcher matcher(NORM_L2);
     std::vector<DMatch> matches;
-    matcher.match(descriptors_img, descriptors_obj, matches);
+    matcher.match(descriptors_obj, descriptors_img, matches);
 
     Mat img_matches;
-    drawMatches(img, keypoints_img, obj, keypoints_obj, matches, img_matches);
+    drawMatches(obj, keypoints_obj, img, keypoints_img, matches, img_matches);
 
         //mark keypoints on images
-    drawKeypoints(img, keypoints_img, akaze_img, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
     drawKeypoints(obj, keypoints_obj, akaze_obj, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
+    drawKeypoints(img, keypoints_img, akaze_img, Scalar(0, 0, 255), DrawMatchesFlags::DEFAULT);
 
     namedWindow("AKAZE image", WINDOW_AUTOSIZE);
     imshow("AKAZE image", akaze_img);
