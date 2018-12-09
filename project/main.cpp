cbd9d789636e81ac1ae8f6c910b52cc240575642
MaximAelterman <maximaelterman@gmail.com>
2018-12-09 17:50:30 +0100

    coindetectie werkt, toevoegen van deathdetectie

--- project/main.cpp
+++ project/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-#define COINPATH "bin/Debug/coin_picked_up_small.png"
+#define COINPATH "bin/Debug/coin_picked_up.png"
+#define DEATHPATH "bin/Debug/death.png"
 
 using namespace std;
 using namespace cv;
 
 int detectCoin(Mat frame);
+int detectDeath(Mat frame);
 
 Mat coin;
+Mat death;
 
 int main(int argc, const char**argv)
 {
@@ -31,8 +34,10 @@ int main(int argc, const char**argv)
 
     VideoCapture vid(vidPath);
     coin = imread(COINPATH);
+    death = imread(DEATHPATH);
 
     cvtColor(coin, coin, COLOR_BGR2GRAY);       //convert coin template into grayscale
+    cvtColor(death, death, COLOR_BGR2GRAY);
 
     if(!vid.isOpened()){
         cout << "Error opening video stream or file" << endl;
@@ -45,6 +50,7 @@ int main(int argc, const char**argv)
         if(frame.empty()) break;
 
         detectCoin(frame);
+        detectDeath(frame);
 
                     //exit when esc is pressed
         char c=(char)waitKey(30);
@@ -55,6 +61,8 @@ int main(int argc, const char**argv)
 
 int detectCoin(Mat frame)
 {
+    static int prevDet;
+    static int coins = 0;
     Mat frame_gray;
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
 
@@ -65,7 +73,7 @@ int detectCoin(Mat frame)
 
     result.create(result_rows, result_cols, CV_32FC1);
 
-        // Do the Matching and Normalize
+        // Do the Matching
     matchTemplate(frame_gray, coin, result, TM_CCOEFF_NORMED);//TM_CCORR_NORMED);
     //normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
@@ -77,15 +85,75 @@ int detectCoin(Mat frame)
 
     minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
 
-    if(maxVal > 0.725)//0.835)
+    if(maxVal > 0.65)
     {
             //rectangle around the found object
-        rectangle(frame, maxLoc, Point( maxLoc.x + coin.cols , maxLoc.y + coin.rows ), Scalar(0, 255, 0), 2, 8, 0);
+        rectangle(frame, maxLoc, Point( maxLoc.x + coin.cols , maxLoc.y + coin.rows), Scalar(0, 255, 0), 2, 8, 0);
         Vec3b punt = result.at<Vec3b>(maxLoc);
-        cout << maxVal << endl;
+
+        if(prevDet == 0)       //prevent multiple detections of the same coin
+        {
+            cout << maxVal << endl;
+            coins++;
+        }
+
+        prevDet = 3;
+    }
+    else if(prevDet > 0)
+    {
+        prevDet--;
     }
 
+    putText(frame, "Coins: "+to_string(coins), Point(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
     imshow("Mario", frame);
-    imshow("result", result);
+    return 0;
+}
+
+int detectDeath(Mat frame)
+{
+    static int prevDet;
+    static int deaths = 0;
+    Mat frame_gray;
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+
+        // Create the result matrix
+    int result_cols = frame_gray.cols - coin.cols + 1;
+    int result_rows = frame_gray.rows - coin.rows + 1;
+    Mat result;
+
+    result.create(result_rows, result_cols, CV_32FC1);
+
+        // Do the Matching
+    matchTemplate(frame_gray, death, result, TM_CCOEFF_NORMED);
+
+            // Localizing the best match with minMaxLoc
+    double minVal;
+    double maxVal;
+    Point minLoc;
+    Point maxLoc;
+
+    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+    if(maxVal > 0.65)
+    {
+            //rectangle around the found object
+        rectangle(frame, maxLoc, Point( maxLoc.x + death.cols , maxLoc.y + death.rows), Scalar(0, 255, 0), 2, 8, 0);
+        Vec3b punt = result.at<Vec3b>(maxLoc);
+
+        if(prevDet == 0)       //prevent multiple detections of the same coin
+        {
+            cout << maxVal << endl;
+            deaths++;
+        }
+
+        prevDet = 3;
+    }
+    else if(prevDet > 0)
+    {
+        prevDet--;
+    }
+
+    putText(frame, "Deaths: "+to_string(deaths), Point(30, 50), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
+    imshow("Mario", frame);
     return 0;
 }
